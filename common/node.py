20bbacdea58fa095a8ae2151f7050897523bec2b
Segretin Louis <louis.segretin@gmail.com>
2019-01-07 15:56:22 +0100

    Petites modifs

--- common/node.py
+++ common/node.py
@@ -4,9 +4,13 @@ Definition de la classe Node pour modele hierachique
 
 class Node:
 
-	def __init__(self, parent):
+	def __init__(self, parent = None):
          self.parent = parent
 		 self.fils = []
 		 self.translate = []
 		 self.rotate = []
 
+	@staticmethod
+    def createStructureFromBVH(file_name):
+        with open(file_name) as file: # Use file to refer to the file object
+			data = file.read()
